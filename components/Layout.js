ce50bfe6862b00b9fe91617d566a6330340b7f1a
sahajranipa <sahajranipa@gmail.com>
2024-01-31 11:10:20 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import styles from "../styles/Layout.module.css";
+import style from "../styles/Layout.module.css";
 const Layout = ({ children }) => {
   return (
     <>

a87d9e203cd22b4cb6f0a8af57005c004feaa174
sahajranipa <sahajranipa@gmail.com>
2024-01-31 10:53:44 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,27 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import styles from "../styles/Layout.module.css";
 const Layout = ({ children }) => {
   return (
-    <div>
-      <header
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}>
-        <h1 style={{ marginRight: "80%" }}>My Blog</h1>
-        <nav className={styles.navbar}>
-          <a className={styles.navlinks} href="/">
-            Home
-          </a>
-          <a className={styles.navlinks} href="/blog">
-            Blogs
-          </a>
-        </nav>
-      </header>
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container className={style.nav_container}>
+          <h1>
+            <Navbar.Brand href="/">Nextjs Blogs</Navbar.Brand>
+          </h1>
+          <Nav className="me-auto navlinks">
+            <Nav.Link style={{ marginRight: "3em" }} href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/blog">Blogs</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
       <main>{children}</main>
-    </div>
+    </>
   );
 };
 
